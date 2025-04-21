f081addee3c0ec326cc2bf27e05a41dbfb44b1df
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2025-04-21 20:26:19 +0300

    🔧 init coursework main.py

---
+++ сoursework/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
